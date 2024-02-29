8b19d7bb437310228ae05d9eda98d6ba59a698a4
Abhinav Singhal <abhinavsinghal876@gmail.com>
2024-02-29 12:19:31 +0530

    Updated LoginActivity.kt

--- app/src/main/java/com/abhinav/jdm/LoginActivity.kt
+++ app/src/main/java/com/abhinav/jdm/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.abhinav.jdm
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +14,49 @@ class LoginActivity : AppCompatActivity(){
         title="Login"
 
         val login=findViewById<Button>(R.id.login_button)
+        val validUserPN="0123456789"
+        val validPassword=arrayOf("Supra","GTR","BMW","Mazda","Honda")
 
         login.setOnClickListener {
             val userpn=findViewById<EditText>(R.id.user_pn).text.toString()
             val userpass=findViewById<EditText>(R.id.user_pass).text.toString()
-            if (userpn.isEmpty() || userpass.isEmpty()) {
-                Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show()
+            val intent=Intent(this,MainActivity::class.java)
+            val nameofJDM: String
+
+            if(userpn==validUserPN){
+                when (userpass) {
+                    validPassword[0] -> {
+                        nameofJDM="Supra"
+                        intent.putExtra("NAME",nameofJDM)
+                        startActivity(intent)
+                    }
+                    validPassword[1] -> {
+                        nameofJDM="GTR"
+                        intent.putExtra("NAME",nameofJDM)
+                        startActivity(intent)
+                    }
+                    validPassword[2] -> {
+                        nameofJDM="BMW"
+                        intent.putExtra("NAME",nameofJDM)
+                        startActivity(intent)
+                    }
+                    validPassword[3] -> {
+                        nameofJDM="Mazda"
+                        intent.putExtra("NAME",nameofJDM)
+                        startActivity(intent)
+                    }
+                    validPassword[4] -> {
+                        nameofJDM="Honda"
+                        intent.putExtra("NAME",nameofJDM)
+                        startActivity(intent)
+                    }
+                }
+            }
+            else if (userpn.isEmpty() || userpass.isEmpty()) {
+                Toast.makeText(this, "Please enter credentials", Toast.LENGTH_SHORT).show()
             }
             else{
-                Toast.makeText(this,"Login Successfull",Toast.LENGTH_SHORT).show()
-                val intent=Intent(this,MainActivity::class.java)
-                startActivity(intent)
+                Toast.makeText(this,"Incorrect Credentials",Toast.LENGTH_SHORT).show()
             }
         }
     }
