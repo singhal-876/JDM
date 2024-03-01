3893a79f7c6b028ed750afa34c964186464ca0b7
Abhinav Singhal <abhinavsinghal876@gmail.com>
2024-03-01 11:20:35 +0530

    Updated MainActivity.kt

--- app/src/main/java/com/abhinav/jdm/MainActivity.kt
+++ app/src/main/java/com/abhinav/jdm/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.abhinav.jdm
 
-import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var titlename: String? = "JDM"
-    lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        sharedPreferences=getSharedPreferences(getString(R.string.Preferences_file_name), Context.MODE_PRIVATE)
+        sharedPreferences=getSharedPreferences(getString(R.string.Preferences_file_name), MODE_PRIVATE)
         setContentView(R.layout.activity_main)
 
         titlename=sharedPreferences.getString("title","JDM")
-
         title=titlename
+
+        val logout=findViewById<Button>(R.id.logout_button)
+        logout.setOnClickListener {
+            Toast.makeText(this,"You have been logged out successfully",Toast.LENGTH_SHORT).show()
+            val intent=Intent(this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
