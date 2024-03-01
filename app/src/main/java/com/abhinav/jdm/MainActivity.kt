b7e0f279cbb583703e9bc35db16d821c1ab529ed
Abhinav Singhal <abhinavsinghal876@gmail.com>
2024-03-01 08:15:13 +0530

    Updated MainActivity.kt

--- app/src/main/java/com/abhinav/jdm/MainActivity.kt
+++ app/src/main/java/com/abhinav/jdm/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.abhinav.jdm
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var titlename: String? = "JDM"
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences=getSharedPreferences(getString(R.string.Preferences_file_name), Context.MODE_PRIVATE)
         setContentView(R.layout.activity_main)
 
-        if(intent!=null){
-            titlename=intent.getStringExtra("NAME")
-        }
+        titlename=sharedPreferences.getString("title","JDM")
 
         title=titlename
     }
